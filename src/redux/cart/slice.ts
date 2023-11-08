31b1d2607f7b834c11ab377136d43ee8ce8cfca1
Daniel <danielstankevich@gmail.com>
2023-11-08 15:34:43 +0300

    The components Cart, CartItem, and cartSlice were created

--- src/redux/cart/slice.ts
+++ src/redux/cart/slice.ts
@@ -31,11 +31,38 @@ const cartSlice = createSlice({
                 return obj.count  + sum
             }, 0)
         },
+
+        minusItem(state, action: PayloadAction<MyActionPayload>) {        
+            const findItem = state.items.find((obj) => obj.id === action.payload.id )
+
+            if (findItem && findItem.count > 1 ) {
+                findItem.count--  
+            }
+            state.totalPrice = calcTotalPrice(state.items)
+        }, 
+
+
+
+        
+
+        removeItem(state, action: PayloadAction<number>) {
+            state.items  = state.items.filter((obj) => obj.id  != action.payload)
+            state.totalPrice = calcTotalPrice(state.items)
+        },
+
+
+
+
+
+        clearItem(state) {
+            state.items = []
+            state.totalPrice = 0
+        },
         
   }
 })
 
 
-export const { addItem } = cartSlice.actions
+export const { addItem,  minusItem,  removeItem, clearItem } = cartSlice.actions
 export default cartSlice.reducer
 
